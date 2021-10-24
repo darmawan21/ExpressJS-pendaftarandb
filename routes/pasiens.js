c114e120ac896a8f3f28ce4157b965098140f4d0
darmawan21 <d.darmawan2111@gmail.com>
2021-10-25 03:56:52 +0700

    menambahkan crud pada tabel pasien

--- routes/pasiens.js
+++ routes/pasiens.js
@@ -2,9 +2,77 @@ var express = require('express');
 var router = express.Router();
 var Pasiens = require("../models/pasiens");
 
-/* GET users listing. */
+/*TAMPIL DATA PASIEN. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Pasiens.findAndCountAll().then(data => {
+    res.json({
+      status: true,
+      pesan: "Berhasil Tampil",
+      data:data.rows,
+      count: data.count
+    });
+  }).catch(salahnya=>{
+    res.json({
+      status: false,
+      pesan: "Gagal Tampil: " + salahnya.message,
+      data: []
+    });
+  });
+});
+
+/* TAMBAH DATA PASIEN. */
+router.post('/', function(req, res, next) {
+  Pasiens.create(req.body).then(data => {
+    res.json({
+      status: true,
+      pesan: "Berhasil Ditambah",
+      data:data
+    });
+  }).catch(salahnya=>{
+    res.json({
+      status: false,
+      pesan: "Gagal Tampil: " + salahnya.message,
+      data: req.body
+    });
+  });
+});
+
+/* UBAH DATA PASIEN. */
+router.put('/', function(req, res, next) {
+  Pasiens.update(req.body, {
+    where : {id:req.body.id}
+  }).then(data => {
+    res.json({
+      status: true,
+      pesan: "Berhasil Ubah",
+      data:data
+    });
+  }).catch(salahnya=>{
+    res.json({
+      status: false,
+      pesan: "Gagal Ubah: " + salahnya.message,
+      data:req.body
+    });
+  });
+});
+
+/* HAPUS DATA PASIEN. */
+router.delete('/', function(req, res, next) {
+  Pasiens.destroy({
+    where : {id:req.body.id}
+  }).then(data => {
+    res.json({
+      status: true,
+      pesan: "Berhasil Hapus",
+      data:data
+    });
+  }).catch(salahnya=>{
+    res.json({
+      status: false,
+      pesan: "Gagal Hapus: " + salahnya.message,
+      data:req.body
+    });
+  });
 });
 
 module.exports = router;
