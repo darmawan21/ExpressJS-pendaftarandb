22b68d700098fbdc80c01dbb6fe56f4a8b3c96f7
darmawan21 <d.darmawan2111@gmail.com>
2021-10-25 03:43:11 +0700

    menambahkan koneksi model ke route

--- koneksi.js
+++ koneksi.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('pendaftarandb', 'root', '', {
     host: 'localhost',
-    dialect: 'mariadb'
+    dialect: 'mysql'
 });
 
 try {
     sequelize.authenticate();
+    sequelize.sync({ alter: true });
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
