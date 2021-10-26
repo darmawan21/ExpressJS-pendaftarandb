d8f66b0f192be24b8aab250ec105a7a8e785d6f0
darmawan21 <d.darmawan2111@gmail.com>
2021-10-26 14:31:54 +0700

    mengganti primary key pasien

--- models/pasiens.js
+++ models/pasiens.js
@@ -2,6 +2,13 @@ const { Sequelize, DataTypes } = require('sequelize');
 var koneksi = require('../koneksi.js');
 
 const Pasien = koneksi.define('Pasien', {
+  
+  no_rm: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    autoIncrement: false
+  },
+
   nama: {
     type: DataTypes.STRING,
     allowNull: false
